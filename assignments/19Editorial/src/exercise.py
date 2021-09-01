703c76b3a417b60b5d8b36cda6daaff1aa474fea
Gabriel Juarez <A01411618@itesm.mx>
2021-09-01 01:00:15 -0500

    actualicé el problema 19

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,7 +1,12 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
-
+    from math import ceil
+    palabras = int(input("Dame el número de palabras: "))
+    paginas = ceil(palabras/475)
+    costo = paginas * 60
+    costo_1 = costo * .9
+    
+    print("El costo de la publicación es :", costo_1)
 
 if __name__ == '__main__':
     main()
